5c6ac8685983cbafa0dc9eaf6985fc18b66eaa14
kishan-zalariya <kishanzalariya7600@gmail.com>
2021-03-28 16:31:15 +0530

    3 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,35 +1,94 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 import BlogCard from "./BlogCard";
 import { isArrayEmpty } from "./Utils";
 
-export default function App() {
-  const blogArr = [
-    {
-      id: 1,
-      title: "Blog Title 1",
-      description:
-        "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-    },
-    {
-      id: 2,
-      title: "Blog Title 2",
-      description:
-        "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-    },
-    {
-      id: 3,
-      title: "Blog Title 3",
-      description:
-        "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-    }
-  ];
+// export default function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showBlogs: true,
+      blogArr: [
+        {
+          id: 1,
+          title: "Blog Title 1",
+          description:
+            "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+          likeCount: 0
+        },
+        {
+          id: 2,
+          title: "Blog Title 2",
+          description:
+            "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+          likeCount: 0
+        },
+        {
+          id: 3,
+          title: "Blog Title 3",
+          description:
+            "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+          likeCount: 0
+        }
+      ]
+    };
 
-  const blogCars = isArrayEmpty(blogArr)
-    ? []
-    : blogArr.map((item, pos) => {
-        return <BlogCard key={pos} blogDetail={item} />;
-      });
+    console.log("Inside constructor");
+  }
 
-  return <div className="app">{blogCars}</div>;
+  onHideBtnClick = () => {
+    this.setState((prevState, prevProps) => {
+      return { showBlogs: !prevState.showBlogs };
+    });
+  };
+  onLikeBtnClick = pos => {
+    const updatedBlogList = this.state.blogArr;
+    let updatedBlogObj = updatedBlogList[pos];
+
+    updatedBlogObj.likeCount = updatedBlogObj.likeCount + 1;
+    updatedBlogList[pos] = updatedBlogObj;
+
+    this.setState({ blogArr: updatedBlogList });
+  };
+
+  shouldComponentUpdate() {
+    console.log("Inside shouldCompnentUpdate");
+    return true;
+  }
+  componentDidMount() {
+    console.log("Component Did Mount");
+  }
+  componentWillUnmount() {
+    console.log("component Unmounting");
+  }
+  componentDidUpdate() {
+    console.log("Inside compnentDidUpdate");
+  }
+
+  render() {
+    console.log("Render called for AppJs");
+    const blogCars = isArrayEmpty(this.state.blogArr)
+      ? []
+      : this.state.blogArr.map((item, pos) => {
+          return (
+            <BlogCard
+              key={pos}
+              blogDetail={item}
+              onLikeBtnClick={() => this.onLikeBtnClick(pos)}
+            />
+          );
+        });
+
+    return (
+      <div className="app">
+        <button onClick={this.onHideBtnClick}>
+          {this.state.showBlogs ? "Hide List" : "Show List"}
+        </button>
+        <br />
+        {this.state.showBlogs ? blogCars : null}
+      </div>
+    );
+  }
 }
+export default App;
--- src/BlogCard.js
+++ src/BlogCard.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 // import "./BlogCard.css";
 import classes from "./BlogCard.module.css";
 import { dumpLogs } from "./Utils";
 
-const BlogCard = ({ blogDetail }) => {
-  dumpLogs(blogDetail);
+/* Function Based Component */
+const BlogCard = props => {
+  // dumpLogs(props.blogDetail);
+  console.log("Blog Card Rendered");
 
   return (
     <div className={classes.newbBlogCard}>
-      <h3>{blogDetail.title}</h3>
-      <p>{blogDetail.description}</p>
+      <h3>{props.blogDetail.title}</h3>
+      <p>{props.blogDetail.description}</p>
+
+      <p>
+        Like Count:
+        <span className={classes.likeCount}>{props.blogDetail.likeCount}</span>
+      </p>
+      <button onClick={props.onLikeBtnClick}>Like</button>
     </div>
   );
 };
+
+/* Class Based Component */
+// class BlogCard extends Component {
+//   state = {
+//     likeCount: 0
+//   };
+//   onLikeBtnClick = () => {
+//     this.setState((prevState, prevProps) => {
+//       return { likeCount: prevState.likeCount + 1 };
+//     });
+//   };
+//   render() {
+//     dumpLogs(this.props.blogDetail);
+//     return (
+//       <div className={classes.newbBlogCard}>
+//         <h3>{this.props.blogDetail.title}</h3>
+//         <p>{this.props.blogDetail.description}</p>
+
+//         <p>
+//           Like Count:
+//           <span className={classes.likeCount}>{this.state.likeCount}</span>
+//         </p>
+//         <button onClick={this.onLikeBtnClick}>Like</button>
+//       </div>
+//     );
+//   }
+// }
 export default BlogCard;
